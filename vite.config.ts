4ace38f7e53f012e3bb0d65a042f003554762f6b
ralphbetta <ralphbetta@gmail.com>
2024-08-27 04:14:25 +0100

    app ready to be tesed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import { glob } from 'glob'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob.sync('lib/**/*.{ts,tsx}', {
+          ignore: ["lib/**/*.d.ts"],
+        }).map(file => [
+          // 1. The name of the entry point
+          relative(
+            'lib',
+            file.slice(0, file.length - extname(file).length)
+          ),
+          // 2. The absolute path to the entry file
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      }
+    }
+  }
 })
